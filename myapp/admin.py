78099c812e3e2f1292ce36e72ffbf324c29d33d6
Jheann21 <maria.jeannlapore@gmail.com>
2026-02-15 13:11:00 +0800

    Activity4

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Task  # import your Task model
+from .models import Task  # import your Task model 
 
 # Register the model
 admin.site.register(Task)
--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)               # task title
+    title = models.CharField(max_length=200)               # task title 
     description = models.TextField(blank=True, null=True)  # task description
     created_at = models.DateTimeField(auto_now_add=True)   # auto timestamp when created
     completed = models.BooleanField(default=False)        # task completion status
