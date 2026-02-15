ddaa3dcfa6bf00dadeda64704fdd68101d8b637a
anjelyn123 <Membrillosanjelyn@gmail.com>
2026-02-15 12:58:14 +0800

    Act4

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Task(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)               # task title
+    description = models.TextField(blank=True, null=True)  # task description
+    created_at = models.DateTimeField(auto_now_add=True)   # auto timestamp when created
+    completed = models.BooleanField(default=False)        # task completion status
 
     def __str__(self):
-        return self.name
+        return self.title
